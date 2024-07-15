492f049cd138690d8610bb88ff3636e86b081c04
Sing Cheng <Sing.cheng@outlook.com>
2024-07-15 17:52:20 -0400

    setup

---
+++ backend/api/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+#test
+# from rest_framework.response import Response
+# from rest_framework.decorators import api_view
+
+# @api_view(['GET'])
+# def hello_world(request):
+#     return Response({"message": "Hello, world!"})
